89a6162a7c9d8d5e9d220135dfff423e5c9e74d4
Keks <keks@htmlacademy.ru>
2015-11-20 05:02:50 +0300

    :kissing_cat: Добавил сборку и ватч для SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
 "use strict";
 
 var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+
+gulp.task("style", function() {
+  return gulp.src("sass/style.{sass,scss}")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: "last 2 versions"})
+    ]))
+    .pipe(gulp.dest("css"));
+});
+
+gulp.task("start", ["style"], function() {
+  gulp.watch("sass/**/*.{sass,scss}", ["style"]);
+});
 
 
 
