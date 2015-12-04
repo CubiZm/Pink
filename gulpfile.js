1ec742f971141c8ccb9268bea41612a6880720f5
CubiZm <seo-xcube@yandex.ru>
2015-12-04 23:40:19 +0300

    add photo-block

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,42 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var browserSync = require('browser-sync').create();
 
 gulp.task("style", function() {
-  return gulp.src("sass/style.{sass,scss}")
-    .pipe(plumber())
-    .pipe(sass()).on('error', sass.logError)
-    .pipe(postcss([
-      autoprefixer({browsers: "last 2 versions"})
-    ]))
-    .pipe(gulp.dest("css"));
+	return gulp.src("sass/style.{sass,scss}")
+		.pipe(plumber())
+		.pipe(sass()).on('error', sass.logError)
+		.pipe(postcss([
+			autoprefixer({browsers: "last 2 versions"})
+		]))
+		.pipe(gulp.dest("css"));
 });
 
 gulp.task("start", ["style"], function() {
-  gulp.watch("sass/**/*.{sass,scss}", ["style"]);
+	gulp.watch("sass/**/*.{sass,scss}", ["style"]);
 });
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+		browserSync.init({
+				server: "./app"
+		});
+
+		gulp.watch("app/scss/*.scss", ['sass']);
+		gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+		return gulp.src("app/scss/*.scss")
+				.pipe(sass())
+				.pipe(gulp.dest("app/css"))
+				.pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
 
 
 
