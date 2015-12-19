1a1f904e67620c08163813846016b6b83580bf1f
CubiZm <seo-xcube@yandex.ru>
2015-12-20 01:34:56 +0300

    script for menu

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@
 
 var parent = document.querySelector(".main-header__section");
 var toggle = document.querySelector(".main-menu__btn");
+var logo = document.querySelector(".logo");
 
 parent.classList.add('js-menu');
 
 toggle.addEventListener("click", function(event) {
 		event.preventDefault();
 		parent.classList.toggle('js-menu--open');
-		toogle.classList.add('js-menu--close');
+		toggle.classList.toggle('btn__js-menu--close');
+		logo.classList.toggle('logo__js-menu--close');
+
 });
 console.log(toggle);
 
