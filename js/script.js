2c7df93d65868f3e5e8738c24677ad417b6db890
CubiZm <seo-xcube@yandex.ru>
2015-12-20 15:44:59 +0300

    Добавила возможный шаблон для mustache, сам mustache + мелкие правки

--- js/script.js
+++ js/script.js
@@ -16,16 +16,33 @@ toggle.addEventListener("click", function(event) {
 console.log(toggle);
 
 
-var close_btn = document.querySelectorAll('.pop-up .pop-up__btn');
+// var close_btn = document.querySelectorAll('.pop-up .pop-up__btn');
 
-for (var i = 0; i < close_btn.length; i++) {
-		var close_btns = close_btn[i];
+// for (var i = 0; i < close_btn.length; i++) {
+// 		var close_btns = close_btn[i];
 
-		close_btns.onclick = function() {
-				var el = this.parentNode;
-				el.parentNode.removeChild(el);
-		};
-}
+// 		close_btns.onclick = function() {
+// 				var el = this.parentNode;
+// 				el.parentNode.removeChild(el);
+// 		};
+// }
+
+var open = document.querySelector('.form__btn');
+var popup = document.querySelector(".pop-up");
+var close = document.querySelector(".pop-up__btn");
+open.addEventListener("click", function() {
+	event.preventDefault();
+	popup.classList.add("js-menu-open");
+	popup.classList.remove("js-close");
+	console.log("клик по кнопке");
+});
+close.addEventListener("click", function(event) {
+event.preventDefault();
+popup.classList.remove("js-menu-open");
+popup.classList.add("js-close");
+});
+
+
 
 
 // //table
