baf790fcc9d7bec96b365d9766e6b55fde4e340b
CubiZm <seo-xcube@yandex.ru>
2015-12-20 21:22:38 +0300

    correct table

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ var logo = document.querySelector(".logo");
 parent.classList.add('js-menu');
 
 toggle.addEventListener("click", function(event) {
-    event.preventDefault();
-    parent.classList.toggle('js-menu--open');
-    toggle.classList.toggle('btn__js-menu--close');
-    logo.classList.toggle('logo__js-menu--close');
+		event.preventDefault();
+		parent.classList.toggle('js-menu--open');
+		toggle.classList.toggle('btn__js-menu--close');
+		logo.classList.toggle('logo__js-menu--close');
 
 });
 
@@ -18,34 +18,34 @@ toggle.addEventListener("click", function(event) {
 
 var tableSlider = document.querySelector('.main-gallery');
 if (tableSlider != null) {
-    var tableRowHidden = tableSlider.querySelector('.price-table__invisible')
-    if (tableSlider != null && window.innerWidth < 660) {
-        var flkty = new Flickity(tableSlider, {
-            // Настройки плагина
-            cellSelector: '.gallery-cell'
-        });
-        flkty.remove(tableRowHidden)
-    }
+		var tableRowHidden = tableSlider.querySelector('.price-table__invisible')
+		if (tableSlider != null && window.innerWidth < 660) {
+				var flkty = new Flickity(tableSlider, {
+						// Настройки плагина
+						cellSelector: '.gallery-cell'
+				});
+				flkty.remove(tableRowHidden)
+		}
 }
 
-
+//maps
 var ymaps;
 if (ymaps != null) {
-    ymaps.ready(function() {
-        var myMap = new ymaps.Map('map', {
-                center: [59.93866675783276, 30.32307250000002], //координаты карты
-                zoom: 16 //приближение карты
-            }),
-            myPlacemark = new ymaps.Placemark([59.938112257845596, 30.32360894180299], {
-                hintContent: 'Nёrds'
-            }, {
-                iconLayout: 'default#image',
-                iconImageHref: '../img/map-marker.svg', //  путь до картинки
-                iconImageSize: [35, 35], //размер картинки
-            });
+		ymaps.ready(function() {
+				var myMap = new ymaps.Map('map', {
+								center: [59.93866675783276, 30.32307250000002], //координаты карты
+								zoom: 16 //приближение карты
+						}),
+						myPlacemark = new ymaps.Placemark([59.938112257845596, 30.32360894180299], {
+								hintContent: 'Nёrds'
+						}, {
+								iconLayout: 'default#image',
+								iconImageHref: '../img/map-marker.svg', //  путь до картинки
+								iconImageSize: [35, 35], //размер картинки
+						});
 
-        myMap.geoObjects.add(myPlacemark);
-    });
+				myMap.geoObjects.add(myPlacemark);
+		});
 }
 
 
@@ -55,27 +55,27 @@ var time = document.querySelector(".js-time");
 var companions = document.querySelector(".js-companions");
 
 function foo(group, initVal) {
-    var minus = group.querySelector(".js-minus");
-    var plus = group.querySelector(".js-plus");
-    var amount = group.querySelector("[type=number]");
+		var minus = group.querySelector(".js-minus");
+		var plus = group.querySelector(".js-plus");
+		var amount = group.querySelector("[type=number]");
 
-    amount.value = initVal;
+		amount.value = initVal;
 
-    minus.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (amount.value > 0) {
-            amount.value--;
-        };
-    });
+		minus.addEventListener("click", function(event) {
+				event.preventDefault();
+				if (amount.value > 0) {
+						amount.value--;
+				};
+		});
 
-    plus.addEventListener("click", function(event) {
-        event.preventDefault();
-        amount.value++;
-    })
+		plus.addEventListener("click", function(event) {
+				event.preventDefault();
+				amount.value++;
+		})
 };
 if (time != null && companions != null) {
-    foo(time, "10");
-    foo(companions, "2");
+		foo(time, "10");
+		foo(companions, "2");
 }
 
 
@@ -86,86 +86,86 @@ var popup = document.querySelector(".pop-up");
 var close = document.querySelector(".pop-up__btn");
 
 if (open != null && popup != null && close != null) {
-    open.addEventListener("click", function() {
-        popup.classList.add("js-menu-open");
-        popup.classList.remove("js-close");
-        console.log("клик по кнопке");
-    });
-    close.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.remove("js-menu-open");
-        popup.classList.add("js-close");
-    });
+		open.addEventListener("click", function() {
+				popup.classList.add("js-menu-open");
+				popup.classList.remove("js-close");
+				console.log("клик по кнопке");
+		});
+		close.addEventListener("click", function(event) {
+				event.preventDefault();
+				popup.classList.remove("js-menu-open");
+				popup.classList.add("js-close");
+		});
 }
 
 
 (function() {
 
-    if (!("FormData" in window)) {
-        return;
-    }
-    var form = document.querySelector(".form");
-    if (form != null) {
-        var elements = form.querySelectorAll(".form input[type=text], form input:checked, .form input[type=number]");
-        var qs = "";
+		if (!("FormData" in window)) {
+				return;
+		}
+		var form = document.querySelector(".form");
+		if (form != null) {
+				var elements = form.querySelectorAll(".form input[type=text], form input:checked, .form input[type=number]");
+				var qs = "";
 
 
-        for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-            var name = element.name;
-            var value = element.value;
-            qs = qs + encodeURIComponent(name) + "=" + encodeURIComponent(value) + "&";
-        }
-
-
-
-
-
-        function preview(file) {
-            var area = document.querySelector(".photo-action__photo-preview");
-            var template = document.querySelector("#image-template").innerHTML;
-            if (file.type.match(/image.*/)) {
-                var reader = new FileReader();
-                reader.addEventListener("load", function(event) {
-                    var html = Mustache.render(template, {
-                        "image": event.target.result,
-                        "name": file.name
-                    });
-                    area.innerHTML = area.innerHTML + html;
-                });
-                reader.readAsDataURL(file);
-            }
-        }
-
-        form.querySelector("#upload-images-btn").addEventListener("change", function() {
-            var files = this.files;
-            for (var i = 0; i < files.length; i++) {
-                preview(files[i]);
-            }
-            this.value = "";
-        });
-
-
-        form.addEventListener("submit", function(event) {
-            event.preventDefault();
-            console.log('lol');
-            var data = new FormData(form);
+				for (var i = 0; i < elements.length; i++) {
+						var element = elements[i];
+						var name = element.name;
+						var value = element.value;
+						qs = qs + encodeURIComponent(name) + "=" + encodeURIComponent(value) + "&";
+				}
 
 
 
-            var xhr = new XMLHttpRequest();
-            xhr.open("get", "thankyou.html", "https://echo.htmlacademy.ru/adaptive?" + (new Date()).getTime());
 
-            xhr.addEventListener("readystatechange", function() {
-                if (xhr.readyState == 4) {
-                    button.classList.add('js-menu-open');
-                    button.classList.remove('js-close');
-                    button.show = '.pop-up';
-                    console.log(xhr.responseText);
-                }
-            });
-            xhr.send(data);
-        });
 
-    }
+				function preview(file) {
+						var area = document.querySelector(".photo-action__photo-preview");
+						var template = document.querySelector("#image-template").innerHTML;
+						if (file.type.match(/image.*/)) {
+								var reader = new FileReader();
+								reader.addEventListener("load", function(event) {
+										var html = Mustache.render(template, {
+												"image": event.target.result,
+												"name": file.name
+										});
+										area.innerHTML = area.innerHTML + html;
+								});
+								reader.readAsDataURL(file);
+						}
+				}
+
+				form.querySelector("#upload-images-btn").addEventListener("change", function() {
+						var files = this.files;
+						for (var i = 0; i < files.length; i++) {
+								preview(files[i]);
+						}
+						this.value = "";
+				});
+
+
+				form.addEventListener("submit", function(event) {
+						event.preventDefault();
+						console.log('lol');
+						var data = new FormData(form);
+
+
+
+						var xhr = new XMLHttpRequest();
+						xhr.open("get", "thankyou.html", "https://echo.htmlacademy.ru/adaptive?" + (new Date()).getTime());
+
+						xhr.addEventListener("readystatechange", function() {
+								if (xhr.readyState == 4) {
+										button.classList.add('js-menu-open');
+										button.classList.remove('js-close');
+										button.show = '.pop-up';
+										console.log(xhr.responseText);
+								}
+						});
+						xhr.send(data);
+				});
+
+		}
 })();
