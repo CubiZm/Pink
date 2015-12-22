e067b1df07be0a7266193e4e2dea0cba947e624b
CubiZm <seo-xcube@yandex.ru>
2015-12-22 03:29:44 +0300

    added automatization file

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,17 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var combineMq = require("gulp-combine-mq");
 var browserSync = require('browser-sync').create();
 
+gulp.task("build", function() {
+	["style", "images", "script", "html"];
+});
+
 gulp.task("style", function() {
-	return gulp.src("sass/style.{sass,scss}")
+	return gulp.src("source/sass/style.{sass,scss}")
 		.pipe(plumber())
 		.pipe(sass({
 			outputStyle: 'expanded'
@@ -16,13 +23,42 @@ gulp.task("style", function() {
 		.pipe(postcss([
 			autoprefixer({browsers: "last 2 versions"})
 		]))
-		.pipe(gulp.dest("css"))
+		.pipe(combineMq())
+		.pipe(rename({
+				suffix: ".min"
+		}))
+		.pipe(gulp.dest("build/css"))
 		.pipe(browserSync.stream());
+});
+
+gulp.task("images", function() {
+	return gulp.src("source/img/*.{png,jpg,gif,svg}")
+		.pipe(imagemin())
+		.pipe(gulp.dest("build/img"));
+});
+
+gulp.task("script", function() {
+	return gulp.src("source/js/*.js")
+		.pipe(rename("script.js"))
+		.pipe(gulp.dest("build/js"))
+		.pipe(rename({
+			suffix: ".min"
+		}))
+		.pipe(gulp.dest("build/js"))
 });
 
 gulp.task("start", ["style"], function() {
 	gulp.watch("sass/**/*.{sass,scss}", ["style"]);
 });
+
+
+
+
+
+
+
+
+
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['style'], function() {
